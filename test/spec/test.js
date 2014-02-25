01c8d509b45ddc512bc66ec19e494e7c852fcb76
Danny Callaghan <dannycallaghan@gmail.com>
2014-02-25 14:49:33 +0000

    Initial commit

---
+++ test/spec/test.js
@@ -0,0 +1,23 @@
+/*global describe, it */
+'use strict';
+
+(function () {
+    
+    describe("Basic Suite", function() {
+	    it("Should pass a basic truthiness test.", function() {
+	        expect(true).toEqual(true);
+	    });
+
+	    it("Should fail when it hits an inequal statement.", function() {
+	        expect(1+1).toEqual(3);
+	    });
+	});
+
+	describe("Another Suite", function() {
+	    it("Should pass this test as well.", function() {
+	        expect(0).toEqual(0);
+	    });
+	});
+
+
+})();
